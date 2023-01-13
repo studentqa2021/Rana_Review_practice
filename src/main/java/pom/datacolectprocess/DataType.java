23747d7ed780c8e9843191ee9178e93db0bd8fa5
studentqa2021 <sarowerhome@gmail.com>
2023-01-12 23:21:26 -0500

    first commit

---
+++ src/main/java/pom/datacolectprocess/DataType.java
@@ -0,0 +1,43 @@
+package pom.datacolectprocess;
+
+public class DataType {
+
+//	this is a java line comment
+//	thijjhdhbnbssjdjjjjj this is garbage
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x =1;
+		int y =2;
+		int z =3;
+		 x =5;
+		 y =2;
+//		 double datatype 
+		 double a = 1.89999999999999999;
+		 double b = 0.50;
+		 double c = 1;
+//		 character datatype 
+		 char f ='r';
+//		 string is a non primitive data type. string is a class
+		 String name ="Rana";
+//		 boolean datatype 
+		 boolean h = true;
+		 boolean j = false;
+		 System.out.println("my name is" + " " + name);
+		 
+		 
+		 
+		 
+		
+	}
+
+	
+	
+	public static void addition() {
+		
+	}
+	
+	
+}
